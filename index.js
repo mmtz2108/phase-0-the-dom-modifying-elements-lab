5970d1dd362a88c77d1134798ecdba5a0979f17d
Marcos Martinez <mmtz2108@gmail.com>
2022-04-11 16:46:27 -0500

    commit tree 63d0a7f8585dee5bd500afa9a12ba42289986051

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+//document.body.append(element);//
+const newHeader = document.createElement('h1');
+//element.append(h1, id="victory");//
+newHeader.id = "victory";
+newHeader.textContent = "Marcos is the champion";
